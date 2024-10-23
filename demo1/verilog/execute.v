fa6787567e479ec98d10129f1f2d39fdd6e67640
dasasish17 <dasasish789@gmail.com>
2024-10-23 15:39:47 -0500

    aditi do you see this

--- demo1/verilog/execute.v
+++ demo1/verilog/execute.v
@@ -5,9 +5,42 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (/* TODO: Add appropriate inputs/outputs for your execute stage here*/);
+module execute (read1Data, read2Data, PC_incr, signExtendedIns10, signExtendedImm, zeroExtendedImm,
+                                 AluSrc1, AluSrc2,
+                                 Oper,
+                                 AluCin, InvA, InvB, Beq, Bne, Blt, Bgt, ALUJump, Zero, Neg,
+                                 nextPC, AluRes, err);
+
 
    // TODO: Your code here
+   parameter OPERAND_WIDTH = 16;
+   input wire [OPERAND_WIDTH-1:0] read1Data, read2Data, PC_incr, signExtendedIns10, signExtendedImm, zeroExtendedImm;
+   input wire [1:0] AluSrc1, AluSrc2;
+   input wire [3:0] Oper;
+   input wire Cin, InvA, InvB, Beq, Bne, Blt, Bgt, Zero, Neg, Ofl, Cout;
+   output wire [OPERAND_WIDTH-1:0] nextPC, Out;
+   output wire err;
+
+
+   // Instantiate the ALU module
+       alu #(
+           .OPERAND_WIDTH(OPERAND_WIDTH),
+           .NUM_OPERATIONS(NUM_OPERATIONS)
+       ) alu_inst (
+           .InA(read1Data),
+           .InB(read2Data),
+           .Cin(Cin),
+           .Oper(Oper),
+           .invA(InvA),
+           .invB(InvB),
+           .sign(1'b1),
+           .Out(Out),
+           .Ofl(Ofl),
+           .Zero(Zero),
+           .Cout(Cout),
+           .Neg(Neg));
+
+
    
 endmodule
 `default_nettype wire
--- demo1/verilog/inv_logic.v
+++ demo1/verilog/inv_logic.v
@@ -1,6 +1,6 @@
 module inv_logic (input [15:0] InA, input [15:0] InB, input invA, input invB, output [15:0] Aout, output [15:0] Bout);
+    // This is a meaningful comment.
     assign Aout = invA ? ~InA : InA;
     assign Bout = invB ? ~InB : InB;
 endmodule
 
-// This is a meaningful comment.
