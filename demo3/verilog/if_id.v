63aad3dbf3fc34f5f81333b6bf74392611147eb2
Tina Li <tli442@wisc.edu>
2024-12-16 17:17:08 -0600

    add

--- demo3/verilog/if_id.v
+++ demo3/verilog/if_id.v
@@ -1,19 +1,19 @@
 `default_nettype none
 
-module if_id (instruction, PC_updated, clk, rst, if_id_instruction, if_id_PC_updated, flush, stall);
+module if_id (instruction, PC_updated, clk, rst, if_id_instruction, if_id_PC_Updated, flush, stall);
 
     parameter INSTRUCTION_SIZE = 16;
 
     input wire clk, rst, stall, flush;
     input wire [INSTRUCTION_SIZE-1:0] instruction, PC_updated;
-    output wire [INSTRUCTION_SIZE-1:0] if_id_instruction, if_id_PC_updated;
+    output wire [INSTRUCTION_SIZE-1:0] if_id_instruction, if_id_PC_Updated;
 
     wire [INSTRUCTION_SIZE-1:0] inter_instr;
 
     assign inter_instr = flush ? 16'b0000_1000_0000_0000 : instruction;
 
     register instr_register (.out(if_id_instruction), .in(inter_instr), .wr_en(~stall), .clk(clk), .rst(1'b0));
-    register pc_update_register (.out(if_id_PC_updated), .in(PC_updated), .wr_en(~stall), .clk(clk), .rst(rst | flush));
+    register pc_update_register (.out(if_id_PC_Updated), .in(PC_updated), .wr_en(~stall), .clk(clk), .rst(rst | flush));
 
 endmodule
 
